b4f7a43b2f50900eebcbd865a9e54142586fe384
Jedediah Arnold <jarnold910@gmail.com>
2018-11-08 12:54:42 -0500

    rules, rule, topics, topic haha

--- src/db/models/rules.js
+++ src/db/models/rules.js
@@ -1,23 +1,23 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Rules = sequelize.define('Rules', {
+  var Rule = sequelize.define('Rule', {
     description: DataTypes.STRING,
     topicId: {
     	type: DataTypes.STRING,
     	onDelete: "CASCADE",
     	references: {
-    		model: "Topic",
+    		model: "Topics",
     		key: "id",
     		as: "topicId",
     	}
     }
   }, {});
-  Rules.associate = function(models) {
+  Rule.associate = function(models) {
     // associations can be defined here
-    Rules.belongsTo(models.Topic, {
+    Rule.belongsTo(models.Topic, {
     	foreignKey: "topicId",
     	onDelete: "CASCADE",
     });
   };
-  return Rules;
+  return Rule;
 };
