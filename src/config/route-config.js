0884662632238fe3afe8eb10b670cd4d2a6b4bea
Jedediah Arnold <jarnold910@gmail.com>
2018-11-12 11:32:52 -0500

    quick bug on topic

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,10 +1,12 @@
 module.exports = {
 	init(app){
 		const staticRoutes = require("../routes/static");
+		const postRoutes = require("../routes/posts");
 		const topicRoutes = require("../routes/topics");
 		const advertisementRoutes = require("../routes/advertisements");
 
 		app.use(staticRoutes);
+		app.use(postRoutes);
 		app.use(topicRoutes);
 		app.use(advertisementRoutes);
 	}
---
+++ src/controllers/postController.js
@@ -0,0 +1,7 @@
+const postQueries = require("../db/queries.posts.js");
+
+module.exports = {
+	new(req, res, next){
+		res.render("posts/new", {topicId: req.params.topicId});
+	}
+}
