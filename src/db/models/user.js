97a6b30f01c86a8c97870db6d77c527f1d9673e6
Jedediah Arnold <jarnold910@gmail.com>
2018-11-16 22:49:50 -0500

    checkpoint voting: complete

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -34,6 +34,11 @@ module.exports = (sequelize, DataTypes) => {
       as: "comments"
     });
 
+    User.hasMany(models.Vote, {
+      foreignKey: "userId",
+      as: "votes"
+    });
+
   };
   return User;
 };
